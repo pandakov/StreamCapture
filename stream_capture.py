b28adfd70eb572011b598cd7c862c2a42b453277
Victor Pandakov <85847075+pandakov@users.noreply.github.com>
2023-05-07 22:35:38 +0300

    short environment

--- stream_capture.py
+++ stream_capture.py
@@ -6,8 +6,9 @@ import schedule
 import time
 from os import getenv
 
+
 def get_stream_frame(stream_url: str) -> cv2.UMat:
-    '''Get frame from stream'''
+    """Get frame from stream"""
     cap = cv2.VideoCapture(stream_url)
     if not cap.isOpened():
         print("Cannot load stream")
@@ -16,64 +17,72 @@ def get_stream_frame(stream_url: str) -> cv2.UMat:
     cap.release()
     return frame
 
+
 def save_stream_frame(stream_url: str, path: str) -> bool:
-    '''Save frame from stream to file'''
+    """Save frame from stream to file"""
     frame = get_stream_frame(stream_url)
     if frame is None:
         return False
-    cv2.imwrite(path, frame)
+    try:
+        cv2.imwrite(path, frame)
+    except Exception as e:
+        print("Error saving image: ", e)
+        return False
     return True
 
+
 def send_stream_frame(stream_url: str, bot: telepot.Bot, chat_id: str) -> bool:
-    '''Send frame from stream by bot'''
-    captured = save_stream_frame(stream_url, 'tmp.jpg')
+    """Send frame from stream by bot"""
+    captured = save_stream_frame(stream_url, "tmp.jpg")
     if not captured:
         return False
     try:
-        bot.sendPhoto(chat_id, 'tmp.jpg')
+        bot.sendPhoto(chat_id, "tmp.jpg")
     except Exception as e:
-        print('Error sending photo: ', e)
+        print("Error sending photo: ", e)
         return False
     return True
 
-def job(stream_url: str, bot: telepot.Bot, chat_id: str, img_path: str, timezone: str):
-    '''Job for scheduler'''
+
+def job(
+    stream_url: str,
+    bot: telepot.Bot,
+    chat_id: str,
+    img_path: str = "img",
+    timezone: str = "Europe/Moscow",
+):
+    """Job for scheduler"""
     dt_string = datetime.now(ZoneInfo(timezone)).strftime("%Y-%m-%d__%H-%M")
     fname = f"{img_path}/{dt_string}.jpg"
-    frame = get_stream_frame(stream_url)
-    if frame is not None:
+    captured = save_stream_frame(stream_url, fname)
+    if captured:
         try:
-            cv2.imwrite(fname, frame)
+            bot.sendPhoto(chat_id, open(fname, "rb"))
         except Exception as e:
-            print('Error saving photo: ', e)
-        try:
-            bot.sendPhoto(chat_id, open(fname, 'rb'))
-        except Exception as e:
-            print('Error sending photo: ', e)
+            print("Error sending photo: ", e)
     else:
         print("Cannot load stream")
 
+
 if __name__ == "__main__":
     # Load environment variables
-    print('Loading environment variables...', end=' ')
-    bot_token = str(getenv('BOT_TOKEN'))
-    chat_id = str(getenv('CHAT_ID'))
-    stream_url = str(getenv('STREAM_URL'))
-    img_path = str(getenv('IMG_PATH'))
-    timezone = str(getenv('TIMEZONE'))
-    capture_time = str(getenv('CAPTURE_TIME'))
-    print('done')
+    print("Loading environment variables...", end=" ")
+    bot_token = str(getenv("BOT_TOKEN"))
+    chat_id = str(getenv("CHAT_ID"))
+    stream_url = str(getenv("STREAM_URL"))
+    capture_time = str(getenv("CAPTURE_TIME"))
+    print("done")
 
     # Create bot
-    print('Creating bot...', end=' ')
+    print("Creating bot...", end=" ")
     bot = telepot.Bot(bot_token)
-    print('done')
+    print("done")
 
     # Schedule
-    schedule.every().day.at(capture_time).do(job, stream_url, bot, chat_id, img_path, timezone)
+    schedule.every().day.at(capture_time).do(job, stream_url, bot, chat_id)
 
     # Scheduler
-    print('Starting scheduler...')
+    print("Starting scheduler...")
     while True:
         schedule.run_pending()
         time.sleep(10)
